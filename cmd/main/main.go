d9e01a5d9db96865b973bd9f84af57f3d4af49ec
Guilherme Martins Valente <guimartins.m.v@gmail.com>
2025-09-06 18:13:01 -0300

    structs correctly exported for the cmd binary

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,6 +6,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Hi, mom!")
 	employee.RepoTest()
+
+	job1 := employee.Job{JobName: "Dev", Salary: 3000, YearsReq: 2}
+	employee1 := employee.Employee{Id: 2, Name: "Gui", Job: job1}
+
+	fmt.Printf("Hi my name is %v, I am a %v and I earn %v \n", employee1.Name, employee1.Job.JobName, employee1.Job.Salary)
+
+	total := 15 * 20 / 100
+
+	fmt.Println(total)
 }
